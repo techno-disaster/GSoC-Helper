ade395aea35685a75c5d1c75b8d02f16d8d00563
Jayesh Nirve <nitinnirve@gmail.com>
2021-02-01 18:12:07 +0530

    redirect url for oauth

--- app.py
+++ app.py
@@ -76,3 +76,13 @@ handler = SlackRequestHandler(app)
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
+
+
+@flask_app.route("/slack/install", methods=["GET"])
+def install():
+    return handler.handle(request)
+
+
+@flask_app.route("/slack/oauth_redirect", methods=["GET"])
+def oauth_redirect():
+    return handler.handle(request)
